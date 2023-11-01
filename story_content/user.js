1f29ea256e5f1036f85f045fc493527ffde9c8e7
Darryl Palaubsanon <dpalaubsanon@gmail.com>
2023-11-01 21:31:38 +0800

    v1

---
+++ story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "63WpaizK0fI":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.getElementById('volume').style.display = 'none';
+}
+
